b24f1f9f967549e84aa5fca6b5b923e14860510f
Rattapoom Ruengkla <rattapoom.rue@gmail.com>
2021-06-07 16:26:08 +0700

    up

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,10 +16,10 @@ export default {
     },
 
     // Global CSS: https://go.nuxtjs.dev/config-css
-    css: ['@/assets/css/main.css'],
+    css: [],
 
     // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-    plugins: [{ src: '~/plugins/jarallax.js', ssr: false }],
+    plugins: [],
 
     // Auto import components: https://go.nuxtjs.dev/config-components
     components: true,
