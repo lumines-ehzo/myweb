c2a0f00a6e43f4ec960784c99ef6ef1d74bd9412
Rattapoom Ruengkla <rattapoom.rue@gmail.com>
2021-06-08 11:58:59 +0700

    change navbar

--- layouts/navbar.vue
+++ layouts/navbar.vue
@@ -3,8 +3,8 @@
       <b-navbar type="dark" variant="dark">
           <b-navbar-nav>
               <nuxt-link class="nav-item nav-link" to="/">Home</nuxt-link>
-              <nuxt-link class="nav-item nav-link" to="/users">Profile</nuxt-link>
-              <nuxt-link class="nav-item nav-link" to="/parallax">Parallax-test</nuxt-link>
+              <nuxt-link class="nav-item nav-link" to="/users">Parallax-test</nuxt-link>
+              <nuxt-link class="nav-item nav-link" to="/parallax">Profile</nuxt-link>
           </b-navbar-nav>
       </b-navbar>
       <nuxt/>
